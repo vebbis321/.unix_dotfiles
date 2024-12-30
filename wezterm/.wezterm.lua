a70e987dc0701621911ed563065303db33510502
vebbis321 <57636432+vebbis321@users.noreply.github.com>
2024-12-30 20:29:11 +0100

    bug

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -14,7 +14,9 @@ config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.8
 config.window_close_confirmation = "NeverPrompt"
 
-config.send_composed_key_when_left_alt_is_pressed = true
+-- config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_left_alt_is_pressed = false
+config.send_composed_key_when_right_alt_is_pressed = true
 config.enable_kitty_graphics = true
 
 return config
