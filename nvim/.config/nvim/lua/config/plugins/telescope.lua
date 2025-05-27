480c53a4eba10742cece0159a9a6e3a1c6dd1912
thel <57636432+vebbis321@users.noreply.github.com>
2025-05-27 11:16:38 +0200

    added telescope keybindings

--- nvim/.config/nvim/lua/config/plugins/telescope.lua
+++ nvim/.config/nvim/lua/config/plugins/telescope.lua
@@ -12,5 +12,18 @@ return {
 
 		local key = vim.keymap
 		key.set("n", "<space>ff", builtin.find_files)
+		key.set("n", "<space>fh", builtin.help_tags)
+		key.set("n", "<space>fs", builtin.live_grep)
+		key.set("n", "<space>fd", builtin.diagnostics)
+
+		key.set("n", "<space>fr", function()
+			builtin.find_files({ cwd = "~/repos/", prompt_title = "repos" })
+		end)
+		key.set("n", "<space>fc", function()
+			builtin.find_files({ cwd = "~/code/", prompt_title = "code" })
+		end)
+		key.set("n", "<space>fn", function()
+			builtin.find_files({ cwd = "~/notes/", prompt_title = "notes" })
+		end)
 	end,
 }
