82c825d9f08a400e86c6776dbf01c8c6109e87b8
thel <57636432+vebbis321@users.noreply.github.com>
2025-05-27 13:11:49 +0200

    added neogen

--- nvim/.config/nvim/lua/config/plugins/mason.lua
+++ nvim/.config/nvim/lua/config/plugins/mason.lua
@@ -1,24 +1,24 @@
 return {
-    "mason-org/mason.nvim",
-    dependencies = {
-        "mason-org/mason-lspconfig.nvim",
-        "WhoIsSethDaniel/mason-tool-installer.nvim"
-    },
-    config = function()
-        require("mason").setup {}
-        require("mason-lspconfig").setup {
-            automatic_enable = false,
-            ensure_installed = {
-                "lua_ls",
-                "basedpyright",
-                "marksman",
-            },
-        }
-        require("mason-tool-installer").setup({
-            ensure_installed = {
-                "stylua", -- lua formatter
-                "ruff",   -- python linter + formatter
-            },
-        })
-    end
+	"mason-org/mason.nvim",
+	dependencies = {
+		"mason-org/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		require("mason").setup({})
+		require("mason-lspconfig").setup({
+			automatic_enable = false,
+			ensure_installed = {
+				"lua_ls",
+				"basedpyright",
+				"marksman",
+			},
+		})
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				"stylua", -- lua formatter
+				"ruff", -- python linter + formatter
+			},
+		})
+	end,
 }
