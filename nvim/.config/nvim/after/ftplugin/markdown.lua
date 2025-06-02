e9ba53b066e80dfe0119a238f1c163a892d72aa6
thel <57636432+vebbis321@users.noreply.github.com>
2025-06-02 10:57:32 +0200

    fixed bug

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -9,7 +9,7 @@ opt.conceallevel = 2
 vim.diagnostic.enable(false)
 
 -- spell
-vim.keymap.set({ "n", "i", "v" }, "<leader>tg", function()
+vim.keymap.set("n", "<leader>tg", function()
 	vim.diagnostic.enable(not vim.diagnostic.is_enabled())
 	vim.opt.spell = not (vim.opt.spell:get())
 end)
