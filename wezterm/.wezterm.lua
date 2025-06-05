be905fafa62e1d7871ffefb081f9cf475d631b0d
thel <57636432+vebbis321@users.noreply.github.com>
2025-06-05 17:09:34 +0200

    added wezterm back

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,30 @@
+local wezterm = require("wezterm")
+local theme = require("wezterm-dragon")
+
+local config = wezterm.config_builder()
+
+local is_linux = function()
+	return wezterm.target_triple:find("linux") ~= nil
+end
+
+if is_linux() then
+	config.dpi = 96
+end
+
+config.font = wezterm.font("Hack Nerd Font Mono")
+config.font_size = 22
+
+config.colors = theme
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.8
+config.window_close_confirmation = "NeverPrompt"
+
+-- config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_left_alt_is_pressed = false
+config.send_composed_key_when_right_alt_is_pressed = true
+config.enable_kitty_graphics = true
+
+return config
